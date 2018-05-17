709644d65552d9e2b021d9100e8ace8fa017d399
Michael McLaren <m.mclaren42@gmail.com>
2018-05-17 13:32:02 -0400

    Revised metaphlan output parsing functions.
    - remade marker_length dataframe to include truncated and non-truncated
    forms of gene ids, to fix an issue with some markers not getting matched
    - reorganized parsing functions
    - sped up the implementation by reducing to just one call of `left_join`

--- R/metaphlan.R
+++ R/metaphlan.R
@@ -1,46 +1,59 @@
-
 # TODO: allow passing a list of file names and sample name (or perhaps a named
 # list of file names)
-parse_clade_profiles <- function (fn, filter = TRUE, combine_markers = TRUE) {
+parse_clade_profiles <- function (fn, combine_markers = TRUE) {
+    ## Helper functions
+    parse_clade <- function (li) {
+        # li is a list of two lines from the metaphlan2 clade_profiles output
+        ## Parse the two lines for this taxon.
+        # The first line is a tab-separated list of the gene identifiers, with the
+        # first position empty. The second line is a tab-separated list of the
+        # clade name followed by the normalized gene abundance (number of reads per
+        # bp * 1000)
+        li.split <- li %>% 
+            map(str_split, "\t", simplify = TRUE)
+        clade <- li.split[[2]][1]
+        genes <- li.split[[1]][-1]
+        abundances = as.double(li.split[[2]][-1])
+        tibble(Clade = clade,
+            Gene = genes, Abundance = abundances)
+    }
+    parse_tax_rank <- function (clade) {
+        # param `clade` is a string of the form
+        # 'k__...|p__...|c__...|o__...|f__...|g__...|s__...|t__...', up to the
+        # taxonomic rank
+        clade %>%
+            str_extract("[k,p,c,o,f,g,s,t](?=__[\\w]+$)") %>%
+            switchv(k = "Kingdom", p = "Phylum", c = "Class", o = "Order", 
+                f = "Family", g = "Genus", s = "Species", t = "Strain")
+    }
+    ## Main
     # Read the clade profiles
     li <- readLines(fn)
     # Drop the header line
     li <- li[-1]
     # Split into clades (pairs of rows)
-    clades <- split(li, (seq(li)+1) %/% 2)
+    clade_lists <- split(li, (seq(li)+1) %/% 2)
     # Parse each clade into a tibble
-    tb <- clades %>%
-        map(parse_clade, combine_markers = combine_markers) %>%
+    tb <- clade_lists %>%
+        map(parse_clade) %>%
         bind_rows
-    # Return the tb with Clade as first column
-    tb %>%
-        select(Clade, Rank, everything())
-}
-
-# li is a list of two lines from the metaphlan2 clade_profiles output
-parse_clade <- function (li, combine_markers = TRUE) {
-    ## Parse the two lines for this taxon.
-    # The first line is a tab-separated list of the gene identifiers, with the
-    # first position empty. The second line is a tab-separated list of the
-    # clade name followed by the normalized gene abundance (number of reads per
-    # bp * 1000)
-    li.split <- li %>% 
-        map(str_split, "\t", simplify = TRUE)
-    clade <- li.split[[2]][1]
-    genes <- li.split[[1]][-1]
-    tax_rank <- clade %>%
-        str_extract("[a-z]__[\\w]+$") %>%
-        str_sub(1, 1) %>%
-        switchv(k = "Kingdom", p = "Phylum", c = "Class", o = "Order", 
-            f = "Family", g = "Genus", s = "Species", t = "Strain")
-    abundances = as.double(li.split[[2]][-1])
-    ## Build a data frame of marker gene info for the clade
-    # The gene lengths are stored in the marker_lengths df; the number of reads
-    # mapped for the gene can be back-calculated from the normalized abundance.
-    tb <- tibble(Clade = clade, Rank = tax_rank, 
-        Gene = genes, Abundance = abundances) %>%
-        left_join(marker_lengths, by = "Gene") %>%
+    # Add the smallest taxonomic rank of the clade
+    tb <- tb %>%
+        mutate(Rank = parse_tax_rank(Clade))
+    # Get gene lengths
+    tb <- tb %>%
+        left_join(marker_lengths, 
+            by = "Gene",
+            suffix = c("", ".db")) %>%
         mutate(Reads = Abundance * Length / 1000)
+    # Reads should all be integers unless something went wrong
+    reads_are_ints <- tb %$% all(abs(Reads - round(Reads)) < 1e-4)
+    if (reads_are_ints) {
+        tb <- tb %>%
+            mutate(Reads = as.integer(Reads))
+    } else {
+        warning("Reads may not all be integers")
+    }
     # Combine markers
     if (combine_markers) {
         tb <- tb %>%
@@ -49,5 +62,7 @@ parse_clade <- function (li, combine_markers = TRUE) {
             mutate(Weight = (Reads / Length) * 1000) %>%
             ungroup()
     }
-    tb
+    # Return the tb with Clade as first column
+    tb %>%
+        select(Clade, Rank, everything())
 }
--- data-raw/metaphlan_marker_lengths.R
+++ data-raw/metaphlan_marker_lengths.R
@@ -1,18 +1,27 @@
 ## Metaphlan marker gene lengths
-# Parse the gene ids and lengths from the Metaphlan marker info file
+
+# Example line we need to parse:
+# gi|483970126|ref|NZ_KB891629.1|:c6456-5752	{'ext': set(['GCF_000226995', 'GCF_000355695', 'GCF_000373585']), 'score': 3.0, 'clade': 's__Streptomyces_sp_KhCrAH_244', 'len': 705, 'taxon': 'k__Bacteria|p__Actinobacteria|c__Actinobacteria|o__Actinomycetales|f__Streptomycetaceae|g__Streptomyces|s__Streptomyces_sp_KhCrAH_244'}
+
+# Parse the clade and gene ids and lengths from the Metaphlan marker info file
 li <- readLines(file.path("~/downloads", "mpa_v20_m200_marker_info.txt"))
-marker_lengths <- li %>%
-    str_match("^(\\S+)\\t.*'len': ([0-9]+),.*$") %>%
+marker_lengths_full <- li %>%
+    str_match("^(\\S+)\\t.*'len': ([0-9]+),.*'taxon': '(\\S+)'.*$") %>%
     as_tibble %>%
-    select(Gene = V2, Length = V3) %>%
+    select(Gene = V2, Length = V3, Clade = V4) %>%
     mutate(Length = as.integer(Length))
-# Some gene identifiers have more than one location span, separated by a comma:
-marker_lengths %>%
+# A small fraction of gene identifiers have more than one location span,
+# separated by a comma:
+marker_lengths_full %>%
     filter(str_detect(Gene, ","))
-# The gene identifiers in the clade_profiles don't have this form; they only
-# have the first span (up to and excluding the comma). Ao we'll want to adjust
-# accordingly by matching up to the first comma (or end of string if no commas)
-marker_lengths <- marker_lengths %>%
-    mutate(Gene_org = Gene,
-        Gene = str_extract(Gene, "^.+?(?=,|$)"))
-devtools::use_data(marker_lengths, internal = TRUE)
+# The gene identifiers in the clade_profiles may have this form, but sometimes
+# get truncated at the first comma. To deal with this, we can add copies with
+# the truncated gene ids for these markers.
+marker_lengths_trunc <- marker_lengths_full %>%
+    filter(str_detect(Gene, ",")) %>%
+    mutate(Gene = str_extract(Gene, "^.+?(?=,|$)"))
+marker_lengths_full <- bind_rows(marker_lengths_full, marker_lengths_trunc)
+# Subset to just the Gene ids and lengths for use in parsing clade profiles
+marker_lengths <- marker_lengths_full %>%
+    select(Gene, Length)
+devtools::use_data(marker_lengths, internal = TRUE, overwrite = TRUE)
